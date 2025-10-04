82b7e3584b88098aaea4d8afd35091e5254c454a
Om Rajput <150363099+OmRajput17@users.noreply.github.com>
2025-10-04 11:03:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import os
 
+## Loading the Environment Variables
 from dotenv import load_dotenv
 load_dotenv()
 
